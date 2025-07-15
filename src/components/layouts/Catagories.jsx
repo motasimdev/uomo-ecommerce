513b9434c97e59ec97654b1074163260d2d1e3e1
barakahtrust <barakahtrustbd@gmail.com>
2025-07-15 15:18:43 +0600

    ctgry running

--- src/components/layouts/Catagories.jsx
+++ src/components/layouts/Catagories.jsx
@@ -7,27 +7,41 @@ import catagryTwo from "/src/assets/collection_grid_2.jpg";
 import catagryThree from "/src/assets/collection_grid_3.jpg";
 import catagryFour from "/src/assets/catagryFour.png";
 import Heading from "../Heading";
+import { Link } from "react-router-dom";
 
 const Catagories = () => {
   return (
     <>
       <div className="py-[55px]">
         <Container>
-          <div className={"flex gap-x-7"}>
+          <div className={"relative flex gap-x-7"}>
             <div className="w-[690px]">
               <Image src={catagryOne} alt={"catagryOne"} />
-              <div className="">
-                <Heading
-                  text={"HOT LIST"}
-                  as={"h4"}
-                  className={""}
-                />
-                <h3 className={"text-[26px] text-[#222222] font-medium"}><span className={"text-[26px] text-[#222222] font-bold"}>WOMEN </span>COLLECTION</h3>
+              <div className="absolute bottom-10 left-10">
+                <Heading text={"HOT LIST"} as={"h4"} className={""} />
+                <h3 className={"text-[26px] text-[#222222] font-medium"}>
+                  <span className={"text-[26px] text-[#222222] font-bold"}>
+                    WOMEN{" "}
+                  </span>
+                  COLLECTION
+                </h3>
+                <div className="group relative mt-2">
+                  <Link to={"/"}>
+                    <Heading
+                      text={"SHOP NOW"}
+                      as={"p"}
+                      className={"text-sm text-[#222222] font-medium uppercase"}
+                    />
+                    <span className="block absolute bottom-[-5px] left-0 bg-black w-[50px] h-[2px]">
+                      <span className="block absolute bottom-0 left-[50px] bg-black w-0 h-[2px] group-hover:w-5 transition-[width] ease-in-out duration-300"></span>
+                    </span>
+                  </Link>
+                </div>
               </div>
             </div>
             <div className="w-[690px]">
               <Image src={catagryTwo} alt={"catagryTwo"} />
-              <div className="flex justify-between mt-7">
+              <div className="flex gap-x-7 mt-7">
                 <Image
                   src={catagryThree}
                   alt={"catagryThree"}
