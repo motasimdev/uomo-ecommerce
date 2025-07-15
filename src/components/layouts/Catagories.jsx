98ea375288ee69e3129979786f73052685d15a45
barakahtrust <barakahtrustbd@gmail.com>
2025-07-15 23:22:42 +0600

    catgry done

--- src/components/layouts/Catagories.jsx
+++ src/components/layouts/Catagories.jsx
@@ -33,7 +33,7 @@ const Catagories = () => {
                       className={"text-sm text-[#222222] font-medium uppercase"}
                     />
                     <span className="block absolute bottom-[-5px] left-0 bg-black w-[55px] h-[2px]">
-                      <span className="block absolute bottom-0 left-[55px] bg-black w-0 h-[2px] group-hover:w-5 transition-[width] ease-in-out duration-300"></span>
+                      <span className="block absolute bottom-0 left-[55px] bg-black w-0 h-[2px] group-hover:w-6 transition-[width] ease-in-out duration-300"></span>
                     </span>
                   </Link>
                 </div>
@@ -61,7 +61,7 @@ const Catagories = () => {
                         }
                       />
                       <span className="block absolute bottom-[-5px] left-0 bg-black w-[55px] h-[2px]">
-                        <span className="block absolute bottom-0 left-[55px] bg-black w-0 h-[2px] group-hover:w-5 transition-[width] ease-in-out duration-300"></span>
+                        <span className="block absolute bottom-0 left-[55px] bg-black w-0 h-[2px] group-hover:w-6 transition-[width] ease-in-out duration-300"></span>
                       </span>
                     </Link>
                   </div>
@@ -73,36 +73,60 @@ const Catagories = () => {
                   alt={"catagryThree"}
                   className={"w-[330px]"}
                 />
-                 <div className="absolute bottom-10 left-10">
-                <Heading text={"HOT LIST"} as={"h4"} className={""} />
-                <h3 className={"text-[26px] text-[#222222] font-medium"}>
-                  <span className={"text-[26px] text-[#222222] font-bold"}>
-                    KIDS{" "}
-                  </span>
-                  COLLECTION
-                </h3>
-                <div className="group relative mt-2 w-[82px]">
-                  <Link to={"/"}>
-                    <Heading
-                      text={"SHOP NOW"}
-                      as={"p"}
-                      className={"text-sm text-[#222222] font-medium uppercase"}
-                    />
-                    <span className="block absolute bottom-[-5px] left-0 bg-black w-[55px] h-[2px]">
-                      <span className="block absolute bottom-0 left-[55px] bg-black w-0 h-[2px] group-hover:w-5 transition-[width] ease-in-out duration-300"></span>
+                <div className="absolute bottom-10 left-10">
+                  <Heading text={"HOT LIST"} as={"h4"} className={""} />
+                  <h3 className={"text-[26px] text-[#222222] font-medium"}>
+                    <span className={"text-[26px] text-[#222222] font-bold"}>
+                      KIDS{" "}
                     </span>
-                  </Link>
+                    COLLECTION
+                  </h3>
+                  <div className="group relative mt-2 w-[82px]">
+                    <Link to={"/"}>
+                      <Heading
+                        text={"SHOP NOW"}
+                        as={"p"}
+                        className={
+                          "text-sm text-[#222222] font-medium uppercase"
+                        }
+                      />
+                      <span className="block absolute bottom-[-5px] left-0 bg-black w-[55px] h-[2px]">
+                        <span className="block absolute bottom-0 left-[55px] bg-black w-0 h-[2px] group-hover:w-6 transition-[width] ease-in-out duration-300"></span>
+                      </span>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-              <div className="">
 
-                <Image
-                  src={catagryFour}
-                  alt={"catagryFour"}
-                  className={"w-[330px]"}
-                />
-                
-              </div>
+                <div className="relative">
+                  <Image
+                    src={catagryFour}
+                    alt={"catagryFour"}
+                    className={"w-[330px]"}
+                  />
+                  <div className="absolute bottom-10 left-10">
+                    <h3 className={"text-[26px] text-[#222222] font-medium"}>
+                      <span className={"text-[26px] text-[#222222] font-bold"}>
+                        E-GIFT{" "}
+                      </span>
+                      CARDS
+                    </h3>
+                    <Heading text={"Surprise someone with the gift they really want."} as={"h4"} className={"w-[215px] my-4 leading-6 text-sm text-[#222222] font-normal"} />
+                    <div className="group relative mt-2 w-[82px]">
+                      <Link to={"/"}>
+                        <Heading
+                          text={"SHOP NOW"}
+                          as={"p"}
+                          className={
+                            "text-sm text-[#222222] font-medium uppercase"
+                          }
+                        />
+                        <span className="block absolute bottom-[-5px] left-0 bg-black w-[55px] h-[2px]">
+                          <span className="block absolute bottom-0 left-[55px] bg-black w-0 h-[2px] group-hover:w-6 transition-[width] ease-in-out duration-300"></span>
+                        </span>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
