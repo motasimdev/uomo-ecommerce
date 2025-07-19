8870a62862b90b1774fd026bc49bd72dd7cde5e0
barakahtrust <barakahtrustbd@gmail.com>
2025-07-19 16:57:35 +0600

    product done

--- src/components/SinglePrdct.jsx
+++ src/components/SinglePrdct.jsx
@@ -2,49 +2,55 @@ import React from "react";
 import Image from "./Image";
 
 import prdctOne_O from "/src/assets/product_1.jpg";
+import prdctOne_1 from "/src/assets/product_1-1.jpg";
 import Heading from "./Heading";
 import { Link } from "react-router-dom";
 
-const SinglePrdct = ({
-  imgSrc,
-  imgAlt,
-  imgClassName,
-  mainDivClassName
-}) => {
+const SinglePrdct = ({ imgSrc, imgAlt, imgClassName, mainDivClassName }) => {
   return (
     <>
       <div className={"group relative"}>
-       
-          <Image src={prdctOne_O} alt={"prdctOne_O"} className={"w-[330px] h-[400px] object-cover"}/>
+        <Link>
+          <Image
+            src={prdctOne_O}
+            alt={"prdctOne_O"}
+            className={"w-[330px] h-[400px] object-cover"}
+          />
+          <Image
+            src={prdctOne_1}
+            alt={"prdctOne_O"}
+            className={
+              "w-[330px] h-[400px] object-cover opacity-0 absolute top-0 left-0 group-hover:opacity-100 hover:transition transition-all duration-300"
+            }
+          />
+        </Link>
 
-
-        <div className="w-[280px] py-5 text-center bg-white absolute left-[50%] -translate-x-[50%] bottom-34 opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-in-out">
           <Link to={"/"}>
+        <div className="w-[280px] py-4 text-center bg-white absolute left-[50%] -translate-x-[50%] bottom-34 opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-in-out">
             <Heading
               text={"add to cart"}
               as={"p"}
-              className={"text-[#222222] text-lg font-normal uppercase"}
-              />
-          </Link>
+              className={"text-[#222222] text-lg font-semibold uppercase"}
+            />
         </div>
-
+          </Link>
 
         <div className="">
-           <Heading
-              text={"Dresses"}
-              as={"p"}
-              className={"text-[#767676] text-sm font-normal pt-5 pb-4"}
-              />
-           <Heading
-              text={"Cropped Faux Leather Jacket"}
-              as={"p"}
-              className={"text-[#222222] text-base font-normal"}
-              />
-           <Heading
-              text={"$29"}
-              as={"p"}
-              className={"text-[#222222] text-base font-normal pt-2"}
-              />
+          <Heading
+            text={"Dresses"}
+            as={"p"}
+            className={"text-[#767676] text-sm font-normal pt-5 pb-4"}
+          />
+          <Heading
+            text={"Cropped Faux Leather Jacket"}
+            as={"p"}
+            className={"text-[#222222] text-base font-normal"}
+          />
+          <Heading
+            text={"$29"}
+            as={"p"}
+            className={"text-[#222222] text-base font-normal pt-2"}
+          />
         </div>
         {/* <div className="">
             <Heading text={productCatgry} as={"h3"} className={"text-[#767676] text-sm font-normal"}/>
